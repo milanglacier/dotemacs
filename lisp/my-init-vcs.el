45a9491307aa96250b829ac94c775f1b71335216
Milan Glacier <dev@milanglacier.com>
2023-02-05 00:17:14 -0500

    config(magit): update magit keymap.

--- lisp/my-init-vcs.el
+++ lisp/my-init-vcs.el
@@ -13,12 +13,19 @@
     (my/git-map
         :states '(normal insert visual insert)
         :keymaps 'override
+        "" '(:ignore t :which-key "git")
         "g" #'magit)
 
     :config
     (add-to-list 'display-buffer-alist
                  '("magit:" ;; the main magit dashboard
-                   (display-buffer-in-new-tab))))
+                   (display-buffer-in-new-tab)))
+    (general-define-key
+     :states '(normal motion)
+     :keymaps 'magit-status-mode-map
+     "gt" #'tab-bar-switch-to-next-tab
+     ;; emulate C-g
+     "<escape>" #'transient-quit-one))
 
 (provide 'my-init-vcs)
 ;;; my-init-vcs.el ends here
