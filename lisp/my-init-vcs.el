fc0641761229ad009df6d1034800350f5fa7c180
Milan Glacier <dev@milanglacier.com>
2023-02-17 01:04:15 -0500

    feat(git-gutter): add git-gutter.

--- lisp/my-init-vcs.el
+++ lisp/my-init-vcs.el
@@ -1,6 +1,7 @@
 ;;; my-init-vcs.el -*- lexical-binding: t; -*-
 
 (straight-use-package 'magit)
+(straight-use-package 'git-gutter)
 
 (general-create-definer my/git-map
     :prefix "SPC g"
@@ -15,6 +16,13 @@
         "" '(:ignore t :which-key "git")
         "g" #'magit)
 
+    (setq magit-diff-refine-hunk t ; show granular diffs in selected hunk.
+          ;; Don't autosave repo buffers. This is too magical, and
+          ;; saving can trigger a bunch of unwanted side-effects, like
+          ;; save hooks and formatters. Trust the user to know what
+          ;; they're doing.
+          magit-save-repository-buffers nil)
+
     :config
     (add-to-list 'display-buffer-alist
                  '("magit:" ;; the main magit dashboard
@@ -27,6 +35,31 @@
      "gt" #'tab-bar-switch-to-next-tab
      ;; emulate C-g
      "<escape>" #'transient-quit-one))
+
+(use-package git-gutter
+    :commands (git-gutter:revert-hunk git-gutter:stage-hunk)
+    :hook ((prog-mode . git-gutter-mode)
+           (conf-mode . git-gutter-mode))
+
+    :init
+    (my/git-map
+        :states '(normal insert visual insert)
+        :keymaps 'override
+        "r" #'git-gutter:revert-hunk
+        "s" #'git-gutter:stage-hunk)
+
+
+    :config
+    (general-define-key
+     :states '(normal visual motion)
+     "]h" #'git-gutter:next-hunk
+     "[h" #'git-gutter:previous-hunk)
+
+    (add-to-list 'display-buffer-alist
+                 '("\\*git-gutter"
+                   (display-buffer-below-selected)
+                   (window-height . 0.3)))
+    )
 
 (provide 'my-init-vcs)
 ;;; my-init-vcs.el ends here
