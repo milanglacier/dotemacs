10d1246399cb08c50b53495585a45b2097d0374b
Milan Glacier <dev@milanglacier.com>
2023-03-25 17:17:21 -0400

    fix(aichat): replace tab by spaces to avoid invoking completion.

--- lisp/autoloads/my-apps-autoloads.el
+++ lisp/autoloads/my-apps-autoloads.el
@@ -178,7 +178,10 @@ suffix."
 (defun my:aichat-input-filter (str)
     "In aichat, multi-line input is enclosed using `{}` brackets. It
 is important to note that aichat does not recognize the newline
-character, and it should be replaced with the return character."
+character, and it should be replaced with the return
+character. Besides, tab character is used to invoke completion,
+there's no way to insert a real tab, we can only replace it by spaces"
+    (setq str (replace-regexp-in-string "\t" "        " str))
     (if (string-match "\n" str)
             (progn
                 (setq str (replace-regexp-in-string "\n" "\r" str))
