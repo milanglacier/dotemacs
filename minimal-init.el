3709c9e03ed9870f331811255f7c90dede4d84bf
Milan Glacier <dev@milanglacier.com>
2023-10-21 17:14:40 -0400

    minimal-init: fix issues where some functions are not available.

--- minimal-init.el
+++ minimal-init.el
@@ -5,23 +5,25 @@
 (require 'package)
 (setq package-user-dir "~/Downloads/emacs-package-dir")
 
-(setq package-list '())
+(setq package-list nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 (unless package-archive-contents
-  (package-refresh-contents))
+    (package-refresh-contents))
 
 (dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
+    (unless (package-installed-p package)
+        (package-install package)))
 
 (setq warning-minimum-level :error)
 
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(xterm-mouse-mode 1)
+(dolist (fun '(menu-bar-mode tool-bar-mode scroll-bar-mode))
+    (when (fboundp fun)
+        (funcall fun -1)))
+
+(unless (display-graphic-p)
+    (xterm-mouse-mode 1))
 
 (setq scroll-step 1)
 (setq scroll-conservatively 10000)
