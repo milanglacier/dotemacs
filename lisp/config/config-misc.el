0d855912870a566f56ba89ee74063fc5a44969fa
Milan Glacier <dev@milanglacier.com>
2025-03-10 16:21:51 -0400

    config(vterm): prefer emacs state to insert state for using evil in vterm.

--- lisp/config/config-misc.el
+++ lisp/config/config-misc.el
@@ -83,6 +83,8 @@
         :states '(normal insert motion)
         "t" #'vterm)
 
+    (add-hook 'evil-collection-setup-hook #'mg--vterm-override-evil-collection)
+
     :config
 
     (advice-add #'vterm :around #'call-command-at-project-root)
