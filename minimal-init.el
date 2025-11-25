bb0b1d98125d6a24503ae698c6a61e57937625ca
Milan Glacier <dev@milanglacier.com>
2025-11-25 03:38:46 -0500

    feat(minimal-init): add startup speed benchmark code.
    Measuring the startup speed in `emacs-startup-hook` is more accurate
    then `emacs-init-time` as some package may set hook on
    `after-init-hook`.

--- minimal-init.el
+++ minimal-init.el
@@ -28,3 +28,15 @@
 (setq scroll-step 1)
 (setq scroll-conservatively 10000)
 (setq auto-window-vscroll nil)
+
+(defvar emacs-startup-end-time nil
+    "Time when Emacs finished loading.")
+
+(defun display-emacs-startup-time ()
+    "Display how long Emacs took to start."
+    (interactive)
+    (message "Emacs startup time: %.2f seconds"
+             (float-time (time-subtract emacs-startup-end-time before-init-time))))
+
+(add-hook 'emacs-startup-hook
+          (defun update-emacs-startup-time () (setq emacs-startup-end-time (current-time))))
